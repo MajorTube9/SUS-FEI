b630d852c5534e91227dfeea05707ff0d2881441
MajorTube9 <149215636+MajorTube9@users.noreply.github.com>
2025-05-12 17:34:28 -0300

    Create biblioteca.c

---
+++ biblioteca.c
@@ -0,0 +1,22 @@
+   #include "biblioteca.h"
+#include <stdio.h>
+
+void cadastrar_paciente() {
+    printf("Funcao de cadastro de paciente...\n");
+}
+
+void consultar_paciente() {
+    printf("Funcao de consulta de paciente...\n");
+}
+
+void listar_pacientes() {
+    printf("Funcao de listagem de pacientes...\n");
+}
+
+void atualizar_paciente() {
+    printf("Funcao de atualizacao de paciente...\n");
+}
+
+void remover_paciente() {
+    printf("Funcao de remocao de paciente...\n");
+}
