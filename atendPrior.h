af25a7f0d93f863efebddbec0c0fc3ba02f4136e
MajorTube9 <149215636+MajorTube9@users.noreply.github.com>
2025-05-18 18:28:24 -0300

    Create atendPrior.h

---
+++ atendPrior.h
@@ -0,0 +1,25 @@
+#ifndef ATENDPRIOR_H
+#define ATENDPRIOR_H
+
+#include "structs.h"
+#include "cadastrar.h"
+#include "atendimento.h"
+
+typedef struct {
+    Registro dados[20];
+    int qtde;
+} Heap;
+
+int filho_esq(int pai);
+int filho_dir(int pai);
+int pai(int filho);
+int ultimo_pai(Heap *h);
+void peneirar(Heap *h, int pai);
+void construir(Heap *h);
+void inserir(Heap *h, Registro reg);
+void remover(Heap *h);
+void enfileirar_prior(Fila *fila, Lista *lista, Heap *heap);
+void desenfileirar_prior(Fila *fila);
+
+
+#endif //ATENDPRIOR_H
