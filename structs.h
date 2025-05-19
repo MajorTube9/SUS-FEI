fd0b1edaadfc792c5b6bb0a66482a770aafd0f15
Sergio Fersan <sergiofersan.biz@gmail.com>
2025-05-19 17:14:40 -0300

    Update structs.h

--- structs.h
+++ structs.h
@@ -3,6 +3,8 @@
 
 // Header que armazena as structs universais
 // (As que todos os outros arquivos usam)
+// Melhor assim do que importar uma biblioteca inteira
+// so pra usar as structs
 
 typedef struct {
     int dia;
@@ -17,6 +19,7 @@ typedef struct {
     Data entrada;
 } Registro;
 
+// Structs de pilha
 typedef struct EPilha {
     struct Epilha *anterior;
     struct Epilha *proximo;
@@ -29,6 +32,7 @@ typedef struct {
     int qtde;
 } Pilha;
 
+// Structs de fila
 typedef struct Efila{
     Registro dados;
     struct Efila *proximo;
@@ -40,5 +44,35 @@ typedef struct {
     Efila *tail;
     int qtde;
 } Fila;
+
+// Struct de heap
+typedef struct {
+    Registro dados[20];
+    int qtde;
+} Heap;
+
+// Structs de fila dinamica
+typedef struct Elista {
+    Registro dados;
+    struct Elista *proximo;
+} Elista;
+
+typedef struct {
+    Elista *inicio;
+    int qtde;
+} Lista;
+
+// Structs de arvore binaria de busca
+typedef struct EABB {
+    Registro dados;
+    struct EABB *filhoEsq;
+    struct EABB *filhoDir;
+    struct EABB *pai;
+} EABB;
+
+typedef struct {
+    EABB *raiz;
+    int qtde;
+} ABB;
 
 #endif
