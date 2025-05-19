e110e9508a6278d37e83404c31ef93a42bf267b2
Sergio Fersan <sergiofersan.biz@gmail.com>
2025-05-19 17:14:25 -0300

    Update pesquisa.h

--- pesquisa.h
+++ pesquisa.h
@@ -2,19 +2,6 @@
 #define PESQUISA_H
 
 #include "structs.h"
-#include "cadastrar.h"
-
-typedef struct EABB {
-    Registro dados;
-    struct EABB *filhoEsq;
-    struct EABB *filhoDir;
-    struct EABB *pai;
-} EABB;
-
-typedef struct {
-    EABB *raiz;
-    int qtde;
-} ABB;
 
 ABB *cria_arvore();
 EABB *cria_vertice(Registro reg);
@@ -24,4 +11,4 @@ void liberaNos(EABB *raiz);
 void limpaABB(ABB *arvore);
 void atualizar_arvores(ABB *arvAno, ABB *arvMes, ABB *arvDia, ABB *arvIdade, Lista *lista);
 
-#endif
+#endif //PESQUISA_H
