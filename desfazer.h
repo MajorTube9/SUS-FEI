a5b566e9f533bae3f62a0603d8f2b815e5d9cdef
Sergio Fersan <sergiofersan.biz@gmail.com>
2025-05-19 12:14:17 -0300

    Create desfazer.h

---
+++ desfazer.h
@@ -0,0 +1,14 @@
+#ifndef DESFAZER_H
+#define DESFAZER_H
+
+#include "structs.h"
+
+
+
+EPilha *inicializar_epilha(Registro reg, int operacao);
+Pilha *inicializar_pilha();
+void push(Pilha *pilha, Registro reg, int operacao);
+int desfazerr(Pilha *pilha, Fila *fila);
+void mostrar_pilha(Pilha *pilha);
+
+#endif
