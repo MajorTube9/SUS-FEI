dc0c9b9b08ab220fa9affb38b9e1a9e8cd673fa8
Sergio Fersan <sergiofersan.biz@gmail.com>
2025-05-19 12:16:00 -0300

    desfazer funcionando

--- atendimento.h
+++ atendimento.h
@@ -4,20 +4,11 @@
 #include "structs.h"
 #include "cadastrar.h"
 
-typedef struct Efila{
-    Registro dados;
-    struct Efila *proximo;
-    struct Efila *anterior;
-} Efila;
 
-typedef struct {
-    Efila *head;
-    Efila *tail;
-    int qtde;
-} Fila;
 
 void inicializar_fila();
-void enfileirar(Fila *fila, Lista *lista);
+void enfileirar(Fila *fila, Lista *lista, Pilha *pilha);
+void desenfileirar(Fila *fila, Pilha *pilha);
 void mostrar_fila(Fila *fila);
 
 #endif
